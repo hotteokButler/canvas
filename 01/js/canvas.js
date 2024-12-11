59a2f29d03067fed79e157aec46f761a37aaaf65
hotteokButler <ygs12567@gmail.com>
2024-12-11 23:57:19 +0900

    선 , 원 그리기

--- 01/js/canvas.js
+++ 01/js/canvas.js
@@ -9,13 +9,33 @@ document.addEventListener('DOMContentLoaded', () => {
   $canvas.width = canvasWidth;
   $canvas.height = canvasHeight;
 
-  const ctx = $canvas.getContext("2d");
+  const ctx = $canvas.getContext('2d');
 
-  ctx.fillRect(100,100,150,150);
-  ctx.fillRect(570,0,150,150);
-  ctx.fillRect(400,300,150,150);
-  ctx.fillRect(60,400,150,150);
+  ctx.fillStyle = '#96d8d8a6';
+  ctx.fillRect(100, 100, 150, 150);
+  ctx.fillStyle = '#d8cd96a6';
+  ctx.fillRect(570, 0, 150, 150);
+  ctx.fillStyle = '#d896bea6';
+  ctx.fillRect(400, 300, 150, 150);
 
+  // line
+  ctx.beginPath();
+  ctx.strokeStyle = '#66B366';
+  ctx.stro = '#66B366';
+  ctx.moveTo(150, 300); //시작
+  ctx.lineTo(200, 100); //선을 잇는 점 좌표
+  ctx.stroke(); //그리기
+
+  ctx.beginPath();
+  ctx.strokeStyle = '#EEBC68';
+  ctx.moveTo(200, 100);
+  ctx.lineTo(400, 300);
+  ctx.stroke(); //그리기
+  
+  ctx.beginPath();
+  ctx.arc(300, 300, 40, 0, Math.PI * 2, false);
+  ctx.strokeStyle = "#5d9358"
+  ctx.stroke();
 
   window.addEventListener('resize', () => {
     clearTimeout(canvasEventTimer);
@@ -25,9 +45,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
       $canvas.width = canvasWidth;
       $canvas.height = canvasHeight;
-
     }, 300);
   });
-
-
 });
